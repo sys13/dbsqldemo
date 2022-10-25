7be788650a7f9cc59af39fe812d1b4f7509f939b
Daniel Arrizza <daniel.arrizza@gmail.com>
2022-10-25 10:18:11 -0400

    add recharts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 import { getWarehouseData } from "~/models/warehouse.server";
 
 export async function loader({ request }: LoaderArgs) {
@@ -10,7 +11,15 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
-  console.log(data);
 
-  return <div>Hello {data[0].fare_amount}</div>;
+  return (
+    <div>
+      <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="trip_distance" />
+        <YAxis />
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <Line type="monotone" dataKey="fare_amount" stroke="#8884d8" />
+      </LineChart>
+    </div>
+  );
 }
